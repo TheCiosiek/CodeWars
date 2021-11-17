8c49ef57f43558bfa735bef5649b15f98454a385
TheCiosiek <92461530+TheCiosiek@users.noreply.github.com>
2021-11-17 02:28:59 +0100

    Create Sum_of_Digits.py

---
+++ Novice/Sum_of_Digits.py
@@ -0,0 +1,21 @@
+def digital_root(n):
+    j=2
+    while j>1:
+        j=0
+        sum=0
+        for i in str(n):
+            sum+=int(i)
+            j+=1
+        n=int(n)
+        n=sum
+    return sum
+  
+  """
+  Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+Examples
+
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+"""
