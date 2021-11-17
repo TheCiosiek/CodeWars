bf9be77b2dff09ba21450166e2edc67bae85a017
TheCiosiek <92461530+TheCiosiek@users.noreply.github.com>
2021-11-17 02:26:09 +0100

    Update Fibonacci.py

--- Novice/Fibonacci.py
+++ Novice/Fibonacci.py
@@ -13,6 +13,13 @@ def productFib(prod):
         return [n, next_n, False]
         
         """
+def productFib(prod):
+  a, b = 0, 1
+  while prod > a * b:
+    a, b = b, a + b
+  return [a, b, prod == a * b]        
+        
+        
         The Fibonacci numbers are the numbers in the following integer sequence (Fn):
 
     0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
