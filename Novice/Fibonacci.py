639e4034dea24e1e66d665ea78282ccd3e6b4f09
TheCiosiek <92461530+TheCiosiek@users.noreply.github.com>
2021-11-17 02:19:17 +0100

    Create Fibonacci.py

---
+++ Novice/Fibonacci.py
@@ -0,0 +1,57 @@
+def productFib(prod):
+    pre_n=0
+    n=0
+    next_n=1
+    
+    while prod>next_n*n:
+        pre_n=n
+        n=next_n
+        next_n=pre_n+n
+    if next_n*n==prod:
+        return [n, next_n, True]
+    else:
+        return [n, next_n, False]
+        
+        """
+        The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+    F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+    F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+[F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+
+F(n) being the smallest one such as F(n) * F(n+1) > prod.
+Some Examples of Return:
+
+(depend on the language)
+
+productFib(714) # should return (21, 34, true), 
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return (34, 55, false), 
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+-----
+productFib(714) # should return [21, 34, true], 
+productFib(800) # should return [34, 55, false], 
+-----
+productFib(714) # should return {21, 34, 1}, 
+productFib(800) # should return {34, 55, 0},        
+-----
+productFib(714) # should return {21, 34, true}, 
+productFib(800) # should return {34, 55, false}, 
+"""
